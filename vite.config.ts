4b035d7967e51ea3c44c98010208f37c04549289
Yunchangki <policeyck@gmail.com>
2025-11-17 09:13:12 +0900

    Add build configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,12 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       }
+            build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html')
+        }
+      }
+    },
     };
 });
